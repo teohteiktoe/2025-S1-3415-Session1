8d8ea2dd8491567cb0dfebcee2afd64d9fa759ef
Teoh TeikToe <teohteiktoe@gmail.com>
2025-08-19 03:43:29 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-print('starting flask')
-
 from flask import Flask,render_template,request
 
 app = Flask(__name__)
@@ -7,6 +5,16 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    #database
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return(render_template("dbs.html", r=(-50.6*q)+90.2))
 
 if __name__ == "__main__":
     app.run()
